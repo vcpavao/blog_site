7a1f00be5f288e6bb56881fbbfe8b405fa4d5369
Victor Pavao <vcpavao@gmail.com>
2018-09-27 16:49:16 -0400

    v2.1
    Minor refactoring of names and file paths, UI improvements, progress on commenting feature

--- README.md
+++
@@ -1,2 +0,0 @@
-# blog_site
-In-progress personal website written with MEAN stack and the Semantic UI framework. The blog-posting feature makes use of RESTful routes.
--- app.js
+++ app.js
@@ -1,64 +1,116 @@
 var express = require("express");
 var app = express();
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 //App config
-mongoose.connect("mongodb://localhost/blogs", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/posts", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //Schema config
-var blogSchema = new mongoose.Schema({
+var postSchema = new mongoose.Schema({
    title: String,
    image: String,
    body: String,
    created: {type: Date, default: Date.now}
 });
 
-var Blog = mongoose.model("Blog", blogSchema);
+var Post = mongoose.model("Post", postSchema);
 
 //RESTful routes
 
 app.get("/", function(req, res) {
-   res.redirect("/blogs");
+   res.redirect("/posts");
 });
 //Index route
-app.get("/blogs", function(req, res) {
-   Blog.find({}, function(err, blogs) {
+app.get("/posts", function(req, res) {
+   Post.find({}, function(err, posts) {
       if(err) {
          console.log(err);
       } else {
-         //Render index with blogs data coming from db
-         res.render("index", {blogs: blogs});
+         //Render index with posts data coming from db
+         res.render("posts/index", {posts: posts});
       }
    })
 });
 
 //New route
-app.get("/blogs/new", function(req, res) {
-   res.render("new");
+app.get("/posts/new", function(req, res) {
+   res.render("posts/new");
 });
+
 //Create route
-app.post("/blogs", function(req, res) {
-   Blog.create(req.body.blog, function(err, newBlog) {
+app.post("/posts", function(req, res) {
+   //Remove script tags
+   req.body.post.body = req.sanitize(req.body.post.body);
+   Post.create(req.body.post, function(err, newPost) {
       if(err) {
-         res.render("new");
+         res.render("posts/new");
       } else {
-         res.redirect("/blogs");
+         res.redirect("/posts");
       }
    })
 });
 
 //Show route
-app.get("/blogs/:id", function(req, res) {
-   Blog.findById(req.params.id, function(err, foundBlog) {
+app.get("/posts/:id", function(req, res) {
+   Post.findById(req.params.id, function(err, foundPost) {
       if(err) {
-         res.redirect("/blogs");
+         res.redirect("/posts");
       } else {
-         res.render("show", {blog: foundBlog});
+         res.render("posts/show", {post: foundPost});
       }
    });
+});
+
+//Edit route
+app.get("/posts/:id/edit", function(req, res) {
+   Post.findById(req.params.id, function(err, foundPost) {
+      if(err) {
+         res.redirect("/posts");
+      } else {
+         res.render("edit", {post: foundPost});
+      }
+   });
+});
+
+//Update route -- put request
+app.put("/posts/:id", function(req, res) {
+   Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
+      if(err) {
+         res.redirect("/posts");
+      } else {
+         res.redirect("/posts/" + req.params.id);
+      }
+   });
+});
+
+//Delete route
+app.delete("/posts/:id", function(req, res) {
+   Post.findByIdAndRemove(req.params.id, function(err) {
+      if(err) {
+         res.redirect("/posts");
+      } else {
+         res.redirect("/posts");
+      }
+   });
+});
+
+//-----COMMENT ROUTING
+
+app.get("/posts/:id/comments/new", function(req, res) {
+   Post.findById(req.params.id, function(err, foundPost) {
+      if(err) {
+         res.redirect("/posts/:id");
+      } else {
+         res.render("comments/new", {post: foundPost});
+      }
+   })
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
